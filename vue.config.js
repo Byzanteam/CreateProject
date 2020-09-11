751bc4094cbd0ac28e47a6ec6e16e1b502d32fbd
xanadu-zxl <xanadu.zxl@gamil.com>
2020-09-11 18:16:04 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const BASE_URL = process.env.NODE_ENV === "productiojn" ? "/real_estate" : "/";
+
+module.exports = {
+  devServer: {
+    proxy: {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    }
+  }
+};
